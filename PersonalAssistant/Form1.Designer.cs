a4e7177cf25f0d11d6e41c0fa19aaea2fe36f46b
Giannis Generalis <81437113+ioanngen@users.noreply.github.com>
2023-05-19 00:07:07 +0300

    Added Main Menu VS Solution
    Main Menu(Home) for our Personal Assistant. Very Early Stage. Feel Free to make any changes.

---
+++ PersonalAssistant/Form1.Designer.cs
@@ -0,0 +1,242 @@
+﻿namespace PersonalAssistant
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            button5 = new Button();
+            button6 = new Button();
+            button7 = new Button();
+            button8 = new Button();
+            textBox1 = new TextBox();
+            button9 = new Button();
+            button10 = new Button();
+            button11 = new Button();
+            button12 = new Button();
+            button13 = new Button();
+            button14 = new Button();
+            label1 = new Label();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(12, 409);
+            button1.Name = "button1";
+            button1.Size = new Size(212, 29);
+            button1.TabIndex = 0;
+            button1.Text = "Phone and Messages";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(230, 409);
+            button2.Name = "button2";
+            button2.Size = new Size(212, 29);
+            button2.TabIndex = 1;
+            button2.Text = "Contacts";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(448, 409);
+            button3.Name = "button3";
+            button3.Size = new Size(212, 29);
+            button3.TabIndex = 2;
+            button3.Text = "Weather and Traffic";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            button4.Location = new Point(666, 409);
+            button4.Name = "button4";
+            button4.Size = new Size(212, 29);
+            button4.TabIndex = 3;
+            button4.Text = "Smart Home";
+            button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            button5.Location = new Point(783, 12);
+            button5.Name = "button5";
+            button5.Size = new Size(94, 29);
+            button5.TabIndex = 4;
+            button5.Text = "Settings";
+            button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            button6.Location = new Point(683, 12);
+            button6.Name = "button6";
+            button6.Size = new Size(94, 29);
+            button6.TabIndex = 5;
+            button6.Text = "Navigation";
+            button6.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            button7.Location = new Point(583, 12);
+            button7.Name = "button7";
+            button7.Size = new Size(94, 29);
+            button7.TabIndex = 6;
+            button7.Text = "Projects";
+            button7.UseVisualStyleBackColor = true;
+            // 
+            // button8
+            // 
+            button8.Location = new Point(483, 12);
+            button8.Name = "button8";
+            button8.Size = new Size(94, 29);
+            button8.TabIndex = 7;
+            button8.Text = "Emails";
+            button8.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(296, 336);
+            textBox1.Name = "textBox1";
+            textBox1.PlaceholderText = "Search the Internet";
+            textBox1.Size = new Size(308, 27);
+            textBox1.TabIndex = 8;
+            // 
+            // button9
+            // 
+            button9.Location = new Point(610, 336);
+            button9.Name = "button9";
+            button9.Size = new Size(122, 29);
+            button9.TabIndex = 9;
+            button9.Text = "Search";
+            button9.UseVisualStyleBackColor = true;
+            // 
+            // button10
+            // 
+            button10.Location = new Point(307, 12);
+            button10.Name = "button10";
+            button10.Size = new Size(94, 29);
+            button10.TabIndex = 10;
+            button10.Text = "Music";
+            button10.UseVisualStyleBackColor = true;
+            // 
+            // button11
+            // 
+            button11.Location = new Point(168, 334);
+            button11.Name = "button11";
+            button11.Size = new Size(122, 29);
+            button11.TabIndex = 11;
+            button11.Text = "Shop Online";
+            button11.UseVisualStyleBackColor = true;
+            // 
+            // button12
+            // 
+            button12.Location = new Point(207, 12);
+            button12.Name = "button12";
+            button12.Size = new Size(94, 29);
+            button12.TabIndex = 12;
+            button12.Text = "My Events";
+            button12.UseVisualStyleBackColor = true;
+            // 
+            // button13
+            // 
+            button13.Location = new Point(107, 12);
+            button13.Name = "button13";
+            button13.Size = new Size(94, 29);
+            button13.TabIndex = 13;
+            button13.Text = "News";
+            button13.UseVisualStyleBackColor = true;
+            // 
+            // button14
+            // 
+            button14.Location = new Point(7, 12);
+            button14.Name = "button14";
+            button14.Size = new Size(94, 29);
+            button14.TabIndex = 14;
+            button14.Text = "Reminders";
+            button14.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Century Gothic", 22.2F, FontStyle.Bold, GraphicsUnit.Point);
+            label1.ForeColor = SystemColors.Highlight;
+            label1.Location = new Point(127, 131);
+            label1.Name = "label1";
+            label1.Size = new Size(650, 44);
+            label1.TabIndex = 15;
+            label1.Text = "Welcome to your Personal Assistant";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.ActiveCaption;
+            ClientSize = new Size(889, 450);
+            Controls.Add(label1);
+            Controls.Add(button14);
+            Controls.Add(button13);
+            Controls.Add(button12);
+            Controls.Add(button11);
+            Controls.Add(button10);
+            Controls.Add(button9);
+            Controls.Add(textBox1);
+            Controls.Add(button8);
+            Controls.Add(button7);
+            Controls.Add(button6);
+            Controls.Add(button5);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            FormBorderStyle = FormBorderStyle.FixedToolWindow;
+            Name = "Form1";
+            Text = "Personal Assisant";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private Button button5;
+        private Button button6;
+        private Button button7;
+        private Button button8;
+        private TextBox textBox1;
+        private Button button9;
+        private Button button10;
+        private Button button11;
+        private Button button12;
+        private Button button13;
+        private Button button14;
+        private Label label1;
+    }
+}
